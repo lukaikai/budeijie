9fbe2352ba7682a5f3b6cc4ec3c9fcb04f782aa0
MD313 <md313@MD313matoMacBookPro.local>
2015-09-01 19:06:02 +0800

    初始化项目

---
+++ 百思不得姐/百思不得姐/Classes/Essence-精华/Controller/XKEssenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  XKEssenceViewController.h
+//  百思不得姐
+//
+//  Created by MD313  on 15/9/1.
+//  Copyright (c) 2015年 xiaokai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XKEssenceViewController : UIViewController
+
+@end
---
+++ 百思不得姐/百思不得姐/Classes/Other-其他/XKTabBarController.m
@@ -0,0 +1,28 @@
+//
+//  XKTabBarController.m
+//  百思不得姐
+//
+//  Created by MD313  on 15/9/1.
+//  Copyright (c) 2015年 xiaokai. All rights reserved.
+//
+
+#import "XKTabBarController.h"
+#import "XKEssenceViewController.h"
+#import "XKNewViewController.h"
+#import "XKFriendTrendsViewController.h"
+#import "XKMeViewController.h"
+
+@interface XKTabBarController ()
+
+@end
+
+@implementation XKTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+@end
